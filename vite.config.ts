b02d5477a697bfa968abf8ccb10e35abd3da895b
Ilya Shabanov <ishaba@me.com>
2024-12-14 16:30:10 +0300

    Add src related imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import { defineConfig } from "vitest/config"
+// import path from "path"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "../"),
+  //   },
+  // },
   plugins: [TanStackRouterVite(), react()],
   server: {
     open: true,
