163cb9837a0a4018a719e6bc76a73b4f24df9339
Ilya Shabanov <ishaba@me.com>
2024-12-14 14:32:16 +0300

    Add tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [TanStackRouterVite(), react()],
   server: {
     open: true,
   },
