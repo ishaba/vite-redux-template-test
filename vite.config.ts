6aa8eeec0a17355d7b0cc9ad81ac79c063bcb810
Ilya Shabanov <ishaba@me.com>
2024-12-15 11:21:13 +0300

    Add purge css and inline critical

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
+import PluginCritical from "rollup-plugin-critical"
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import { defineConfig } from "vitest/config"
+import pluginPurgeCss from "@mojojoejo/vite-plugin-purgecss"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [
+    TanStackRouterVite(),
+    react(),
+    pluginPurgeCss({
+      variables: true,
+    }),
+    PluginCritical({
+      criticalUrl: "https://vite-redux-template-test.vercel.app/",
+      criticalBase: "./",
+      criticalPages: [{ uri: "", template: "index" }],
+      criticalConfig: {
+        base: "dist/",
+        inline: true,
+      },
+    }),
+  ],
   server: {
     open: true,
   },
